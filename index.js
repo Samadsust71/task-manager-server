db86c1188f646b165348c86cb79c9e1aaa8be737
Samadsust71 <samadsust71@gmail.com>
2025-02-21 01:07:13 +0600

    added delete route for task

--- index.js
+++ index.js
@@ -78,6 +78,13 @@ async function run() {
         const result = await taskCollection.find().toArray();
         res.send(result);
       });
+
+      app.delete("/task/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
